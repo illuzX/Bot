a824ad79041c097bdd52050870ef265cbd82dc5f
illuzX <98640218+illuzX@users.noreply.github.com>
2026-05-23 23:01:09 +0530

    Refactor environment variable assignments in config.py

--- config.py
+++ config.py
@@ -4,10 +4,18 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-FORCE_SUB_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL"))
-FORCE_SUB_LINK = os.getenv("FORCE_SUB_LINK")
+MONGO_URI = os.getenv("MONGO_URI")
 
-LOG_CHANNEL = os.getenv("LOG_CHANNEL")
-DB_CHANNEL = os.getenv("DB_CHANNEL")
+FORCE_SUB_CHANNEL = int(
+    os.getenv("FORCE_SUB_CHANNEL")
+)
+
+FORCE_SUB_LINK = os.getenv(
+    "FORCE_SUB_LINK"
+)
+
+LOG_CHANNEL = int(
+    os.getenv("LOG_CHANNEL")
+)
 
 ADMINS = [681308121]
