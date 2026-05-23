23aff7b79f4b9cdb1c9a1a335e00ce9b6eb2d749
illuzX <98640218+illuzX@users.noreply.github.com>
2026-05-23 23:47:57 +0530

    Update database.py
    Error

--- utils/database.py
+++ utils/database.py
@@ -1,11 +1,19 @@
-from motor.motor_asyncio import (
-    AsyncIOMotorClient
-)
+from motor.motor_asyncio import AsyncIOMotorClient
 
 from config import MONGO_URI
 
+import certifi
+
 mongo = AsyncIOMotorClient(
-    MONGO_URI
+
+    MONGO_URI,
+
+    tls=True,
+
+    tlsCAFile=certifi.where(),
+
+    serverSelectionTimeoutMS=5000
+
 )
 
 db = mongo["gsm_bot"]
@@ -21,19 +29,27 @@ settings = db["settings"]
 
 async def add_user(user_id):
 
-    if not await users.find_one({
+    user = await users.find_one({
+
         "user_id": user_id
-    }):
+
+    })
+
+    if not user:
 
         await users.insert_one({
+
             "user_id": user_id
+
         })
 
 
 async def is_banned(user_id):
 
     user = await banned.find_one({
+
         "user_id": user_id
+
     })
 
     return bool(user)
@@ -46,25 +62,41 @@ async def search_solution(query):
         "$or": [
 
             {
+
                 "model": {
+
                     "$regex": query,
+
                     "$options": "i"
+
                 }
+
             },
 
             {
+
                 "issue": {
+
                     "$regex": query,
+
                     "$options": "i"
+
                 }
+
             },
 
             {
+
                 "keywords": {
+
                     "$regex": query,
+
                     "$options": "i"
+
                 }
+
             }
 
         ]
+
     })
