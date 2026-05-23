34415b1888e274a7cee87a1227100e8b69021690
illuzX <98640218+illuzX@users.noreply.github.com>
2026-05-23 21:19:44 +0530

    Change LOG_CHANNEL and DB_CHANNEL to string type

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 FORCE_SUB_CHANNEL = int(os.getenv("FORCE_SUB_CHANNEL"))
 FORCE_SUB_LINK = os.getenv("FORCE_SUB_LINK")
 
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
-DB_CHANNEL = int(os.getenv("DB_CHANNEL"))
+LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+DB_CHANNEL = os.getenv("DB_CHANNEL")
 
 ADMINS = [681308121]
